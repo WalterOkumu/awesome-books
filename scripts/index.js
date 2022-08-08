eb65dc19d63c08b6121c16c3999075a342e81690
Okumu Oriaro <okumu.oriaro@gmail.com>
2022-08-08 22:14:55 +0300

    Added field validation

--- scripts/index.js
+++ scripts/index.js
@@ -88,15 +88,20 @@ function addBook() {
   const bookTitle = document.getElementsByClassName('book-title')[0];
   const author = document.getElementsByClassName('author')[0];
 
-  bookList.push({
-    Title: bookTitle.value,
-    Author: author.value,
-  });
+  if (bookTitle.value === '' || author.value === '') {
+    // eslint-disable-next-line no-alert
+    alert('Please make sure to fill both Title & Author fields!');
+  } else {
+    bookList.push({
+      Title: bookTitle.value,
+      Author: author.value,
+    });
 
-  bookTitle.value = '';
-  author.value = '';
+    bookTitle.value = '';
+    author.value = '';
 
-  appendToBook();
+    appendToBook();
+  }
 }
 
 function clearUI() {
