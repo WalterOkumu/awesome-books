d3f9c9e805ff18ce583cf82fa435e9e0c0c1c306
Okumu Oriaro <okumu.oriaro@gmail.com>
2022-08-11 17:42:20 +0300

    Updated active link as per peer-to-peer review

--- scripts/navLinks.js
+++ scripts/navLinks.js
@@ -9,20 +9,29 @@ const sectionContact = document.getElementsByClassName('contact')[0];
 
 function navigateList() {
   sectionList.style.display = 'block';
+  list.className = 'nav-link active';
   sectionAddBook.style.display = 'none';
+  addNew.className = 'nav-link';
   sectionContact.style.display = 'none';
+  contact.className = 'nav-link';
 }
 
 function navigateAddNew() {
   sectionList.style.display = 'none';
+  list.className = 'nav-link';
   sectionAddBook.style.display = 'block';
+  addNew.className = 'nav-link active';
   sectionContact.style.display = 'none';
+  contact.className = 'nav-link';
 }
 
 function navigateContact() {
   sectionList.style.display = 'none';
+  list.className = 'nav-link';
   sectionAddBook.style.display = 'none';
+  addNew.className = 'nav-link';
   sectionContact.style.display = 'block';
+  contact.className = 'nav-link active';
 }
 
 list.onclick = function () {
